f2a49596418c9031be7a7ad594d31e4c38de4d4d
Avashneupane9857 <juisch20220011252@gmail.com>
2023-08-08 14:30:18 +0545

    ecommerce project finished

---
+++ working.js
@@ -0,0 +1,17 @@
+const bar = document.getElementById('bar')
+
+const list = document.querySelector('.list')
+// console.log(list)
+if (bar) {
+    bar.addEventListener('click', () => {
+
+        list.classList.add('active')
+    })
+}
+const close = document.querySelector(".close")
+if (close) {
+    close.addEventListener('click',
+        () => {
+            list.classList.remove('active')
+        })
+}
